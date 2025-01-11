82ebd538ad704f33474b64bd638115aa69c8b915
Yiyang Wang <yy_wang_7@sjtu.edu.cn>
2025-01-11 21:06:13 +0000

    levelkv

--- levelkv/src/host/main.cpp
+++ levelkv/src/host/main.cpp
@@ -1,9 +1,11 @@
 #include "host.hpp"
+#include "comch_common.hpp"
 
 int main() {
-  Host host("b5:00.0", 3);
-  host.DebugPrint();
-  host.Run();
+  // Host host("b5:00.0", 3);
+  // host.DebugPrint();
+  // host.Run();
+  auto cfg = comch_init("Client", "b5:00.0", "", nullptr);
   
   return 0;
 }
