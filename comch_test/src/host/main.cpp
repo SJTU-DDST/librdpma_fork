81668a59c2e2c6d01ed5fe2f3f2b350754ae4d3d
Yiyang Wang <yy_wang_7@sjtu.edu.cn>
2025-02-19 02:23:41 +0000

    comch_test

--- comch_test/src/host/main.cpp
+++ comch_test/src/host/main.cpp
@@ -8,7 +8,7 @@ static void comch_host_recv_callback(doca_comch_event_msg_recv *event,
                                     doca_comch_connection *comch_connection) {
   auto ctx_user_data = doca_comch_connection_get_user_data(comch_connection);
   Comch *comch = reinterpret_cast<Comch *>(ctx_user_data.ptr);
-  if (msg_len == 1 && *recv_buffer = 'E') {
+  if (msg_len == 1 && *recv_buffer == 'E') {
     std::cout << "exit\n";
     finished = true;
   }
