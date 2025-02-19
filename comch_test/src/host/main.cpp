12a18f15b399b9e4fbd006e1b4d4d54ef647f555
Yiyang Wang <yy_wang_7@sjtu.edu.cn>
2025-02-19 10:25:49 +0800

    comch_test

--- comch_test/src/host/main.cpp
+++ comch_test/src/host/main.cpp
@@ -4,22 +4,23 @@
 bool finished = false;
 
 static void comch_host_recv_callback(doca_comch_event_msg_recv *event,
-                                    uint8_t *recv_buffer, uint32_t msg_len,
-                                    doca_comch_connection *comch_connection) {
+                                     uint8_t *recv_buffer, uint32_t msg_len,
+                                     doca_comch_connection *comch_connection) {
   auto ctx_user_data = doca_comch_connection_get_user_data(comch_connection);
   Comch *comch = reinterpret_cast<Comch *>(ctx_user_data.ptr);
   if (msg_len == 1 && *recv_buffer == 'E') {
     std::cout << "exit\n";
     finished = true;
+  } else {
+    char msg[msg_len];
+    comch->Send(msg, msg_len);
   }
-  char msg[msg_len];
-  comch->Send(msg, msg_len);
 }
 
 int main() {
   Comch host_comch(false, "Comch", "b5:00.0", "", comch_host_recv_callback,
-                  comch_send_completion, comch_send_completion_err,
-                  nullptr, nullptr, nullptr);
+                   comch_send_completion, comch_send_completion_err, nullptr,
+                   nullptr, nullptr);
   while (!finished) {
     host_comch.Progress();
   }
